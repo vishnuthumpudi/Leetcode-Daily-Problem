7b7aa3b4dcc01b53c6124af74691b24d29649384
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-03-01 11:01:32 +0530

    Create maximum_odd_binary_number.cpp

---
+++ maximum_odd_binary_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int ones = count(s.begin(), s.end(), '1');
+        int sz = s.size();
+   
+        string res = "1";
+
+        int nonOnes = sz - ones;
+        ones--;
+
+        while(nonOnes-->0){
+            res += "0";
+        }
+        while(ones-->0){
+            res += "1";
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
