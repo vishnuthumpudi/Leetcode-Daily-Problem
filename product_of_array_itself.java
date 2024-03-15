138ab0aadd86f04e03c8cd785bbd71998967d13d
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-03-15 06:56:40 +0530

    Create product_of_array_itself.java

---
+++ product_of_array_itself.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product = 1;
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = product;
+            product = product * nums[i];
+        }
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] = ans[i] * product;
+            product = product * nums[i];
+        }
+        return ans;
+    }
+}
