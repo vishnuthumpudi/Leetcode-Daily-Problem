be21c2348e7ef7b21b2305f9b95be95d37a5289d
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-06-26 12:35:40 +0530

    Create Balance a Binary Search Tree.java

---
+++ Balance a Binary Search Tree.java
@@ -0,0 +1,28 @@
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        inOrder(root);
+        return bst(0, ordered.size() - 1);
+    }
+    private TreeNode bst(int start, int end) {
+        if (start > end) return null;
+        else if (start == end) {
+            TreeNode ret = ordered.get(start);
+            ret.left = null;
+            ret.right = null;
+            return ret;
+        } else {
+            int mid = start + (end - start) / 2;
+            TreeNode ret = ordered.get(mid);
+            ret.left = bst(start, mid - 1);
+            ret.right = bst(mid + 1, end);
+            return ret;
+        }
+    }
+    private void inOrder(TreeNode node) {
+        if (node == null) return;
+        inOrder(node.left);
+        ordered.add(node);
+        inOrder(node.right);
+    }
+    private List<TreeNode> ordered = new ArrayList<>();
+}
