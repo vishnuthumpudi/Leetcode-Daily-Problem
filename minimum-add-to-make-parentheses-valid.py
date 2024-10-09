88d0d3596fc0819851cf6797a168548e78d55706
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-10-09 08:56:52 +0530

    Create minimum-add-to-make-parentheses-valid.py

---
+++ minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_brackets = 0
+        mismatch = 0
+        for char in s:
+            if char == '(':
+                open_brackets += 1
+            else:
+                if open_brackets > 0:
+                    open_brackets -= 1
+                else:
+                    mismatch += 1
+        return open_brackets + mismatch
