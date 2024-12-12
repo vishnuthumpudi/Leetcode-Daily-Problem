1b4ef2feb3da03283eac8a658bc667a477ade73f
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-12-12 09:05:29 +0530

    Create take-gifts-from-the-richest-pile.java

---
+++ take-gifts-from-the-richest-pile.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long pickGifts(int[] g, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int val : g) maxHeap.add(val);
+
+        for (int i = 0; i < k && maxHeap.peek() > 1; i++) {
+            int x = maxHeap.poll();
+            maxHeap.add((int) Math.sqrt(x));
+        }
+
+        // Return sum
+        long sum = 0;
+        for (int val : maxHeap) sum += val;
+        return sum;
+    }
+}
