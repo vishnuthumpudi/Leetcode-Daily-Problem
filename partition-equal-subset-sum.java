2580a51324264ec083bb6566588aadea7d6879a4
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2025-04-07 07:10:30 +0530

    Create partition-equal-subset-sum.java

---
+++ partition-equal-subset-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        if (total % 2 != 0) return false;
+
+        int target = total / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int n : nums) {
+            for (int i = target; i >= n; i--) {
+                if (dp[i]) continue;
+                if (dp[i - n]) dp[i] = true;
+                if (dp[target]) return true;
+            }
+        }
+
+        return false;        
+    }
+}
